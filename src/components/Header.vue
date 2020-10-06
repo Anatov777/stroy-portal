6fd8a40eb5f07cac38d3c723e62272c9c172a93b
Anatov777 <anatovtimur@gmail.com>
2020-10-06 03:00:30 +0300

    add header head

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <div class="header__content">
+      <nav class="nav-primary">
+        <div class="container">
+          <ul class="nav-primary__list">
+            <li>
+              <a href="#">
+                <img src="images/location.svg" alt="location-icon" />
+                <span>Волгоградская область</span>
+              </a>
+            </li>
+            <li><a href="#">Тендеры</a></li>
+            <li><a href="#">База подрядчиков</a></li>
+            <li>
+              <a href="#">
+                <span>Другие сервисы</span>
+                <img src="images/arrow-down.svg" alt="arrow-down-icon" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div class="header__body">Logo</div>
+    </div>
+  </header>
+</template>
